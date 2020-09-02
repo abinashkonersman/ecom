08cccf512978f835fd64942439bc6b0768ef9448
ABINASH BISWAL KONERSMAN <dougabi@yahoo.com>
2020-09-02 00:28:11 -0700

    2nd commit

--- src/App.js
+++ src/App.js
@@ -6,9 +6,14 @@ import SingleRoom from "./pages/SingleRoom";
 import Error from "./pages/Error";
 
 import {Route, Switch} from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <>
+     
+    <Navbar />
     <Switch>
 
    
