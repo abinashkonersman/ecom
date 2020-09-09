a92c494e45d448e8228064f7efbcbbd0e3272ab3
ABINASH BISWAL KONERSMAN <dougabi@yahoo.com>
2020-09-09 14:11:11 -0700

    featured rooms

---
+++ src/components/FeaturedRooms.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import { RoomContext } from "../context";
+import Loading from "./Loading";
+
+import Title from "./Title";
+
+export default class FeaturedRooms extends Component {
+    static contextType = RoomContext;
+    render() {
+
+        const { featuredRooms: rooms} = this.context;
+        console.log(rooms);
+       
+        
+        return (
+            <div>
+               from featured room 
+              
+               <Loading />
+            </div>
+        );
+    }
+}
--- src/components/Services.js
+++ src/components/Services.js
@@ -8,27 +8,27 @@ export default class Services extends Component {
             {
                 icon: <FaCocktail/>,
                 title:"free cocktails",
-                info:'My Property and i would love to show you all!'
+                info:'Every evening, Canopy hotels has free tastings at their hotel bar, where you can sample local wines, beers and spirits at no cost.'
 
             },
 
             {
                 icon: <FaHiking/>,
                 title:"Endless Hiking",
-                info:'My Property and i would love to show you all!'
+                info:'The Endless Wall Trail is a lesser-known trail in the New River Gorge, making it a perfect place for hikers to get away from the crowds and experience the New River Gorges natural beauty in a more remote setting. '
 
             },
 
             {
                 icon: <FaShuttleVan/>,
                 title:"free Shuttle",
-                info:'My Property and i would love to show you all!'
+                info:'Customers must be registered with Paratransit in advance of making a reservation.'
 
             },
             {
                 icon: <FaBeer/>,
                 title:"Strong and dark chilled Beer",
-                info:'My Property and i would love to show you all!'
+                info:'Good news for the beer-lovers out there: you can get free beer. Drinking is expensive. Pre-gaming doesn’t have the appeal as it once did and now that you’re of age, you want to hit the bars with your friends and proudly flaunt that ID of yours.'
 
             }
         ]
@@ -48,5 +48,5 @@ export default class Services extends Component {
             </div>
                </section>
         );
-    }
+    } 
 }
